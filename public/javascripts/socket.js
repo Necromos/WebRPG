c194c5dd53c7bdfcbf6c4c8ddd28a71e4bd981f2
Necromos <masternecromos@gmail.com>
2013-05-23 14:54:24 +0200

    chat remodeled to proper place

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,24 +1,21 @@
 var socket = io.connect('http://localhost:3000');
 
-socket.on('connect', function(){
-    socket.emit('adduser', prompt("What's your name?"), urlTaker('id'));
-});
-
-socket.on('updatechat', function (username, data) {
-    $('#conversation').append('<b>'+username + ':</b> ' + data + '<br>');
-});
-
-$(function(){
-    $('#datasend').click( function() {
-        var message = $('#data').val();
-        $('#data').val('');
-        socket.emit('sendchat', message);
+$(document).ready(function(){
+    socket.on('connect', function(){
+        socket.emit('adduser', prompt("What's your name?"), urlTaker('id'));
     });
 
-    $('#data').keypress(function(e) {
+    socket.on('updatechat', function (username, data) {
+        $('#chatBody').append('<b>'+username + ':</b> ' + data + '<br>');
+        $('#chatBody').scrollTop(9999999);
+    });
+    $('#chatSend').keypress(function(e) {
         if(e.which == 13) {
+            var message = $('#chatSend').val();
+            $('#chatSend').val('');
+            socket.emit('sendchat', message);
             $(this).blur();
-            $('#datasend').focus().click();
+            $('#chatSend').focus();
         }
     });
 });
